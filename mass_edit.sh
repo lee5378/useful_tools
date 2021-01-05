50de3a2102c16e7395655438612b229bce3e990d
David Lee <49456773+lee5378@users.noreply.github.com>
2021-01-04 22:38:45 -0800

    Create mass_edit.sh

---
+++ mass_edit.sh
@@ -0,0 +1,2 @@
+for i in {2..9}; do cp -r ops-301-guide/curriculum/class-0$i/challenges/* ../ops-301-guide/curriculum/class-0$i/challenges/; done
+for i in {10..15}; do cp -r ops-301-guide/curriculum/class-$i/challenges/* ../ops-301-guide/curriculum/class-$i/challenges/; done
